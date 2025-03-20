634e0def6236ae39cde9fda5e6ac253c0343607b
ShaniRubDev <froindshani@gmail.com>
2025-03-20 12:57:30 +0200

    desine website

--- ServerSide/src/index.js
+++ ServerSide/src/index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const fs = require('fs')
 const path = require('path')
-// const conf = require('./config/config')
 const cors = require('cors');
-// const logEvents = require('./loger');
-// const events = require('events');
-// const eventEmitter = new events.EventEmitter();
 const basketRoute = require('./routers/basketRoute')
-// const verifyJWT = require('./middelware/verifyJWT')
 const app = express();
 const session = require('express-session');
-// const { loger } = require('./middelware/logs')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const publicPath = path.join(__dirname, 'public');
 const uploadDir = path.join(__dirname, 'uploads');
+const announcementRoutes = require('./routers/announcementRoute');
+
 
 // הדפס את הנתיב על מנת לוודא שהוא נכון
 console.log("Uploads folder path:", uploadDir);
@@ -35,13 +31,8 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
 }
 
-
-// app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
-// app.use('/uploads', express.static(path.join(__dirname, 'src', 'uploads')));
-// app.use('/uploads', express.static(path.join('uploads')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
+app.use('/announcements', announcementRoutes);
 app.use('/basket', basketRoute)
 app.listen(5000, () => {
     console.log(`app is listenning on port http://localhost:5001`)
