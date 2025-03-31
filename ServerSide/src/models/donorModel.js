1d3633a84a960937469653761eee7dc4003842dc
ShaniRubDev <froindshani@gmail.com>
2025-03-31 12:41:29 +0300

    last tag

--- ServerSide/src/models/donorModel.js
+++ ServerSide/src/models/donorModel.js
@@ -6,7 +6,20 @@ const saveDonor = async (firstName, lastName, phone, email, address) => {
         INSERT INTO donors (first_name, last_name, phone, email, address) 
         VALUES (?, ?, ?, ?, ?)`;
     const params = [firstName, lastName, phone, email, address];
-    return await create_query(query, params);
+
+    // return await create_query(query, params);
+    try {
+        // השתמש ב- get_query להוספה
+        const result = await create_query(query, params);
+    
+
+        // החזרת ה-ID של התורם (insertId של התוצאה)
+        return { id: result.insertId };
+    } catch (error) {
+        console.error("Error saving donor:", error);
+        throw error;
+    }
+
 };
 
 // פונקציה לקרוא את פרטי התורם (אם יש צורך בעתיד)
