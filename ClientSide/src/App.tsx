c4f88f28fb4e926226dba7cd6ea15969797cf775
ShaniRubDev <froindshani@gmail.com>
2025-04-03 14:11:04 +0300

    Update App.tsx

--- ClientSide/src/App.tsx
+++ ClientSide/src/App.tsx
@@ -1,22 +1,15 @@
-
 import React, { useState } from 'react';
-import { HashRouter } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Change import to use BrowserRouter
-// import Login from './components/Login/Login';
-// import Signup from './components/Signup/Signup';
-import { configureStore } from '@reduxjs/toolkit';
-// import userSlice from './redux/features/userSlice';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../src/redux/store'; 
+
+import './App.scss';
+
 import About from './components/About/About';
 import DonationPage from './components/DonationPage/DonationPage';
 import DonationCart from './components/DonationCart/DonationCart';
-import { store, persistor } from '../src/redux/store'; // ייבוא נכון של ה-store וה-persistor
-import AddBasket from './components/AddBasket/AddBasket'; // ייבוא הקומפוננטה החדשה
+import AddBasket from './components/AddBasket/AddBasket';
 import DeleteBasket from './components/DeleteBasket/DeleteBasket';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
@@ -29,59 +22,38 @@ import DonorsList from "./components/DonorsList/DonorsList"
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import ManageAnnouncements from './components/ManageAnnouncements/ManageAnnouncements';
-// import PersonalDetailsForm from './components/PersonalDetailsForm/PersonalDetailsForm';
-
-
-
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, userReducer);
-
-// const store = configureStore({
-//   reducer: {
-//     user: persistedReducer,
-//   },
-// });
-
-// const persistor = persistStore(store);
 
 function App() {
   const [cartVisible, setCartVisible] = useState(false);
 
   return (
-        <HashRouter>
-   <Provider store={store}>
-     <PersistGate loading={null} persistor={persistor}>
-    <Routes>
-       <div className="App" dir="rtl" lang="he">
-           <Route path="/" element={<Layout />}>
-             <Route path='/About' element={<About />} />
-             <Route path='/' element={<Home />} />
-             <Route path="/donation" element={<DonationPage />} />
-             <Route path="/donationCart" element={<DonationCart setCartVisible={setCartVisible} />} />
-             <Route path="/add-basket" element={<AddBasket />} />
-             <Route path="/manage-basket" element={<DeleteBasket />} />
-             <Route path="/manage-area" element={<ManageArea />} />
-             <Route path="/add-masage" element={<AddAnnouncement />} />
-             <Route path="/thenks" element={<ThankYou />} />
-             <Route path="/payment" element={<PersonalDetailsForm />} />
-             <Route path="/DonationsList" element={<DonationsList />} />
-             <Route path="/DonorsList" element={<DonorsList />} />
-             <Route path="/Login" element={<Login />} />
-             <Route path="/register" element={<Register />} />
-             <Route path="/manageAn" element={<ManageAnnouncements />} />
-           </Route>
-       </Routes>
-       </div>
-     </PersistGate>
-   </Provider>
- </HashRouter>
-
-
+    <HashRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <div className="App" dir="rtl" lang="he">
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route path='/About' element={<About />} />
+                <Route path='/' element={<Home />} />
+                <Route path="/donation" element={<DonationPage />} />
+                <Route path="/donationCart" element={<DonationCart setCartVisible={setCartVisible} />} />
+                <Route path="/add-basket" element={<AddBasket />} />
+                <Route path="/manage-basket" element={<DeleteBasket />} />
+                <Route path="/manage-area" element={<ManageArea />} />
+                <Route path="/add-masage" element={<AddAnnouncement />} />
+                <Route path="/thenks" element={<ThankYou />} />
+                <Route path="/payment" element={<PersonalDetailsForm />} />
+                <Route path="/DonationsList" element={<DonationsList />} />
+                <Route path="/DonorsList" element={<DonorsList />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/manageAn" element={<ManageAnnouncements />} />
+              </Route>
+            </Routes>
+          </div>
+        </PersistGate>
+      </Provider>
+    </HashRouter>
   );
 }
 
