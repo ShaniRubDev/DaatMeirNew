1265f27e6b2d7e9b888ef327c5ca563929dfcbf3
ShaniRubDev <froindshani@gmail.com>
2025-04-24 23:01:32 +0300

    Update donationsService.ts

--- ClientSide/src/services/donationsService.ts
+++ ClientSide/src/services/donationsService.ts
@@ -5,6 +5,7 @@
 // export const saveDonations = async (donations: any[]) => {
 //     try {
 //         const response = await axios.post(`${API_URL}/save-donation`, { donations });
+
 //         return response.data;
 //     } catch (error) {
 //         console.error("❌ Error saving donations:", error);
@@ -43,6 +44,7 @@ export const saveDonations = async (donations: any[]) => {
 export const fetchDonations = async () => {
     try {
         const response = await axios.get(`${API_URL}/all`);
+        console.log("🌍 API_URL:", process.env.REACT_APP_API_BASE_URL);
         return response.data;
     } catch (error) {
         console.error("Error fetching donations:", error);
